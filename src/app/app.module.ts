decd2da8dea3b461082945cb7e8d689a25f8ecd5
rajanish007 <ranjan.rajanish0@gmail.com>
2019-01-02 23:35:19 +0530

    Added bootstrap 4.2.1
    Signed-off-by: rajanish007 <ranjan.rajanish0@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {GenericModule} from "./generic/generic.module";
+import {StaticModule} from "./static/static.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GenericModule,
+    StaticModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/static/static.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavbarComponent} from './navbar/navbar.component';
+import {BrowserModule} from "@angular/platform-browser";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule
+  ],
+  declarations: [
+    NavbarComponent
+  ]
+})
+export class StaticModule {
+}
